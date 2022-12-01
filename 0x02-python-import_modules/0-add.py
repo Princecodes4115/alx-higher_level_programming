dc9f654cb456d75832b35c548f6f8cbb8bc75372
Ezekiel Moses <codewithprince.com@gmail.com>
2022-12-01 15:37:44 -0800

    initial commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print some of 1 and 2"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+
